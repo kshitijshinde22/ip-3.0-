311b63c18c780fd75b09b7eb2e19d2f8dd4f39b0
kshitijshinde22 <139535379+kshitijshinde22@users.noreply.github.com>
2024-04-06 11:05:59 +0530

    Add files via upload

---
+++ view_content.php
@@ -0,0 +1,119 @@
+<?php
+
+include '../components/connect.php';
+
+if (isset($_COOKIE['tutor_id'])) {
+   $tutor_id = $_COOKIE['tutor_id'];
+} else {
+   $tutor_id = '';
+   header('location:login.php');
+}
+
+if (isset($_GET['get_id'])) {
+   $get_id = $_GET['get_id'];
+} else {
+   $get_id = '';
+   header('location:contents.php');
+}
+
+if (isset($_POST['delete_video'])) {
+
+   $delete_id = $_POST['video_id'];
+   $delete_id = filter_var($delete_id, FILTER_SANITIZE_STRING);
+
+   $delete_video_thumb = $conn->prepare("SELECT thumb FROM `content` WHERE id = ? LIMIT 1");
+   $delete_video_thumb->execute([$delete_id]);
+   $fetch_thumb = $delete_video_thumb->fetch(PDO::FETCH_ASSOC);
+   unlink('../uploaded_files/' . $fetch_thumb['thumb']);
+
+   $delete_video = $conn->prepare("SELECT video FROM `content` WHERE id = ? LIMIT 1");
+   $delete_video->execute([$delete_id]);
+   $fetch_video = $delete_video->fetch(PDO::FETCH_ASSOC);
+   unlink('../uploaded_files/' . $fetch_video['video']);
+
+   $delete_likes = $conn->prepare("DELETE FROM `likes` WHERE content_id = ?");
+   $delete_likes->execute([$delete_id]);
+   $delete_comments = $conn->prepare("DELETE FROM `comments` WHERE content_id = ?");
+   $delete_comments->execute([$delete_id]);
+
+   $delete_content = $conn->prepare("DELETE FROM `content` WHERE id = ?");
+   $delete_content->execute([$delete_id]);
+   header('location:contents.php');
+
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>view content</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="../css/admin_style.css">
+
+</head>
+
+<body>
+
+   <?php include '../components/admin_header.php'; ?>
+
+
+   <section class="view-content">
+
+      <?php
+      $select_content = $conn->prepare("SELECT * FROM `content` WHERE id = ? AND tutor_id = ?");
+      $select_content->execute([$get_id, $tutor_id]);
+      if ($select_content->rowCount() > 0) {
+         while ($fetch_content = $select_content->fetch(PDO::FETCH_ASSOC)) {
+            $video_id = $fetch_content['id'];
+
+            $count_likes = $conn->prepare("SELECT * FROM `likes` WHERE tutor_id = ? AND content_id = ?");
+            $count_likes->execute([$tutor_id, $video_id]);
+            $total_likes = $count_likes->rowCount();
+            ?>
+            <div class="container">
+               <video src="../uploaded_files/<?= $fetch_content['video']; ?>" autoplay controls
+                  poster="../uploaded_files/<?= $fetch_content['thumb']; ?>" class="video"></video>
+               <h3 class="title">
+                  <?= $fetch_content['title']; ?>
+               </h3>
+               <div class="flex">
+                  <div><i class="fas fa-heart"></i><span>
+                        <?= $total_likes; ?>
+                     </span></div>
+               </div>
+               <div class="description">
+                  <?= $fetch_content['description']; ?>
+               </div>
+               <form action="" method="post">
+                  <div class="flex-btn">
+                     <input type="submit" value="delete" class="delete-btn" onclick="return confirm('delete this video?');"
+                        name="delete_video">
+                  </div>
+               </form>
+            </div>
+            <?php
+         }
+      } else {
+         echo '<p class="empty">no contents added yet! <a href="add_content.php" class="btn" style="margin-top: 1.5rem;">add videos</a></p>';
+      }
+
+      ?>
+
+   </section>
+
+   <?php include '../components/footer.php'; ?>
+
+   <script src="../js/admin_script.js"></script>
+
+</body>
+
+</html>
